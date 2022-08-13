8f70027712eb78323bb88e9c8163f6cf67cd5018
dloppes <d.loppes.soares@gmail.com>
2022-08-13 15:44:40 +0000

    Collect sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -46,8 +46,8 @@ def validate_data(values):
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
-    except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")
+    except ValueError as error:
+        print(f"Invalid data: {error}, please try again.\n")
         return False
 
     return True
@@ -81,6 +81,23 @@ def calculate_surplus_data(sales_row):
    
     return surplus_data
 
+def get_last_5_entries():
+    """
+    Collects columns of data from sales worksheet, collecting
+    the last 5 entries for each sandwich and returns the data
+    as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+
+    columns = []
+    for ind in range(1, 7):
+       column = sales.col_values(ind)
+       columns.append(column[-5:])
+    
+    return columns
+
+   
+
 def main():
     """
     Run all program functions
@@ -95,4 +112,5 @@ def main():
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+sales_columns = get_last_5_entries()
+# main()
